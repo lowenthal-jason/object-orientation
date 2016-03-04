5ff16cf07c3ceac30405d6b1d5aac782b1947890
Jason Lowenthal <jason.lowenthal@macmillan.com>
2016-03-03 22:12:23 -0600

    Removing grade from domain class and setter for the grade in exam service

--- tests/ObjectOrientedBundle/Services/ExamGraderTest.php
+++ tests/ObjectOrientedBundle/Services/ExamGraderTest.php
@@ -31,12 +31,12 @@ class ExamGraderTest extends \PHPUnit_Framework_TestCase
             $resultingGrade = $examGrader->getGradeForExam($examToGrade);
             verify($resultingGrade)->equals($expectedOutput);
         }, ['examples'=>[
-            [new Exam(100, 90, 10, 10, null), 'A'],
-            [new Exam(100, 80, 10, 10, null), 'B'],
-            [new Exam(100, 70, 10, 10, null), 'C'],
-            [new Exam(100, 60, 10, 10, null), 'D'],
-            [new Exam(100, 50, 10, 10, null), 'F'],
-            [new Exam(100, 40, 10, 10, null), 'F'],
+            [new Exam(100, 90, 10, 10), 'A'],
+            [new Exam(100, 80, 10, 10), 'B'],
+            [new Exam(100, 70, 10, 10), 'C'],
+            [new Exam(100, 60, 10, 10), 'D'],
+            [new Exam(100, 50, 10, 10), 'F'],
+            [new Exam(100, 40, 10, 10), 'F'],
         ]]);
     }
 }
